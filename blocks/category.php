9284f4b04f5ae3f4eb57f83973589d9340cf6ba5
natata7 <natata7@gmail.com>
2022-12-05 02:59:42 +0200

    add blocks

---
+++ blocks/category.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Register Block Category.
+ *
+ * @category Plugin
+ * @package  Wpinci
+ * @author   chyta
+ * @license  https://www.gnu.org/licenses/gpl-3.0.html GPL 3
+ */
+
+/**
+ * Add block category WP INCI.
+ *
+ * @param array $categories Block categories array
+ *
+ * @return array
+ */
+function Wi_Blocks_category( $categories )
+{
+    $block_category = [ 'title' => 'WP INCI', 'slug' => 'wp-inci' ];
+    $category_slugs = array_column($categories, 'slug');
+
+    if (! in_array($block_category['slug'], $category_slugs, true) ) {
+        $categories = array_merge(
+            [
+            [
+            'title' => $block_category['title'],
+            'slug'  => $block_category['slug'],
+            ],
+            ],
+            $categories
+        );
+    }
+
+    return $categories;
+}
+
+add_filter('block_categories_all', 'Wi_Blocks_category', 10, 1);
